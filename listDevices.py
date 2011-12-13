3f43dbdc0017905ad683450ce808811b9950723e
fizzisist <fizzisist@fpgamining.com>
2011-12-13 02:51:17 -0800

    Added * to indicate that a device is available (not mining).

--- listDevices.py
+++ listDevices.py
@@ -2,10 +2,14 @@ import d2xx
 
 devices = d2xx.listDevices()
 
-for d, serial in enumerate(devices):
-	if len(devices) > 100:
-		print "%3d %s" % (d, serial)
-	elif len(devices) > 10:
-		print "%2d %s" % (d, serial)
-	else:
-		print "%d %s" % (d, serial)
+for devicenum, serial in enumerate(devices):
+	try: 
+		h = d2xx.open(devicenum)
+		h.close()
+		isopen = True
+	except:
+		isopen = False
+	
+	print "%2d %s %s" % (devicenum, serial, '*' if isopen else '')
+
+print "* means this device is currently available"
